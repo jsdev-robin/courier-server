b742eeb3f185c3c46482a1561429442f2bc7a592
jsdev-robin <jsdev.robin@gmail.com>
2025-12-13 10:47:34 +0600

    feat: add findOneAndUpdateAssignAuto

--- apps/parcel/src/services/ParcelAdminServices.ts
+++ apps/parcel/src/services/ParcelAdminServices.ts
@@ -2,7 +2,7 @@ import { nodeClient } from '@server/cloud';
 import { ApiError } from '@server/middlewares';
 import { catchAsync, HttpStatusCode, Status } from '@server/utils';
 import { NextFunction, Request, RequestHandler, Response } from 'express';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { IParcel } from '../models/parcel/types';
 
 export class ParcelAdminServices {
@@ -18,7 +18,12 @@ export class ParcelAdminServices {
         {
           $and: [{ _id: req.params.id }, { status: 'Booked' }],
         },
-        { $set: { assignedAgent: req.body.agentId, status: 'ASSIGNED' } },
+        {
+          $set: {
+            assignedAgent: new Types.ObjectId(req.params.agentId),
+            status: 'Assigned',
+          },
+        },
         { new: true }
       );
 
@@ -40,10 +45,28 @@ export class ParcelAdminServices {
   );
 
   public findOneAndUpdateAssignAuto: RequestHandler = catchAsync(
-    async (req: Request, res: Response): Promise<void> => {
+    async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+      const parcel = await this.model
+        .findOne({
+          $and: [{ _id: req.params.id }, { status: 'Booked' }],
+        })
+        .lean();
+
+      if (!parcel) {
+        return next(
+          new ApiError(
+            'Parcel not found with the given ID or already assigned',
+            HttpStatusCode.NOT_FOUND
+          )
+        );
+      }
+
       const user = await nodeClient.geoSearch(
         'agent/location',
-        { longitude: 90.01955619163091, latitude: 23.834249431330473 },
+        {
+          longitude: parcel?.deliveryAddress?.coordinates?.lng,
+          latitude: parcel?.deliveryAddress?.coordinates?.lat,
+        },
         { radius: 1000, unit: 'm' },
         {
           SORT: 'ASC',
@@ -51,10 +74,21 @@ export class ParcelAdminServices {
         }
       );
 
+      const agentId = user[0];
+
+      if (!agentId) {
+        return next(
+          new ApiError('No nearby agent found', HttpStatusCode.NOT_FOUND)
+        );
+      }
+
+      parcel.assignedAgent = new Types.ObjectId(agentId);
+      parcel.status = 'Assigned';
+      await parcel.save();
+
       res.status(HttpStatusCode.OK).json({
         status: Status.SUCCESS,
         message: 'Parcel has been auto-assigned successfully.',
-        user,
       });
     }
   );
