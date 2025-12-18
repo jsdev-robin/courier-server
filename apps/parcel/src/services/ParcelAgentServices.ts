e911b6dbc8d387cc318ff8ff9207931008c6d184
jsdev-robin <jsdev.robin@gmail.com>
2025-12-18 12:55:56 +0600

    Fix parcel agent services

--- apps/parcel/src/services/ParcelAgentServices.ts
+++ apps/parcel/src/services/ParcelAgentServices.ts
@@ -14,116 +14,119 @@ export class ParcelAgentServices {
 
   public find: RequestHandler = catchAsync(
     async (req: Request, res: Response): Promise<void> => {
-      const parcels = await this.model.aggregate([
-        {
-          $match: {
-            assignedAgent: new mongoose.Types.ObjectId(req.self.id),
-            status: { $ne: ParcelStatus.BOOKED },
+      const page = Number(req.query.page) || 1;
+      const limit = Number(req.query.limit) || 10;
+      const skip = (page - 1) * limit;
+
+      const parcels = await this.model
+        .aggregate([
+          {
+            $match: {
+              assignedAgent: new mongoose.Types.ObjectId(req.self.id),
+              status: { $ne: ParcelStatus.BOOKED },
+            },
           },
-        },
-        {
-          $addFields: {
-            nextStatuses: {
-              $map: {
-                input: {
-                  $filter: {
-                    input: [
-                      ParcelStatus.ASSIGNED,
-                      ParcelStatus.PICKED_UP,
-                      ParcelStatus.IN_TRANSIT,
-                      ParcelStatus.DELIVERED,
-                      ParcelStatus.FAILED,
-                    ],
-                    as: 'possibleStatus',
-                    cond: {
-                      $gt: [
-                        {
-                          $indexOfArray: [
-                            [
-                              ParcelStatus.BOOKED,
-                              ParcelStatus.ASSIGNED,
-                              ParcelStatus.PICKED_UP,
-                              ParcelStatus.IN_TRANSIT,
-                              ParcelStatus.DELIVERED,
-                              ParcelStatus.FAILED,
-                            ],
-                            '$$possibleStatus',
-                          ],
-                        },
-                        {
-                          $indexOfArray: [
-                            [
-                              ParcelStatus.BOOKED,
-                              ParcelStatus.ASSIGNED,
-                              ParcelStatus.PICKED_UP,
-                              ParcelStatus.IN_TRANSIT,
-                              ParcelStatus.DELIVERED,
-                              ParcelStatus.FAILED,
-                            ],
-                            '$status',
-                          ],
-                        },
+          {
+            $addFields: {
+              nextStatuses: {
+                $map: {
+                  input: {
+                    $filter: {
+                      input: [
+                        ParcelStatus.ASSIGNED,
+                        ParcelStatus.PICKED_UP,
+                        ParcelStatus.IN_TRANSIT,
+                        ParcelStatus.DELIVERED,
+                        ParcelStatus.FAILED,
                       ],
+                      as: 'possibleStatus',
+                      cond: {
+                        $gt: [
+                          {
+                            $indexOfArray: [
+                              [
+                                ParcelStatus.BOOKED,
+                                ParcelStatus.ASSIGNED,
+                                ParcelStatus.PICKED_UP,
+                                ParcelStatus.IN_TRANSIT,
+                                ParcelStatus.DELIVERED,
+                                ParcelStatus.FAILED,
+                              ],
+                              '$$possibleStatus',
+                            ],
+                          },
+                          {
+                            $indexOfArray: [
+                              [
+                                ParcelStatus.BOOKED,
+                                ParcelStatus.ASSIGNED,
+                                ParcelStatus.PICKED_UP,
+                                ParcelStatus.IN_TRANSIT,
+                                ParcelStatus.DELIVERED,
+                                ParcelStatus.FAILED,
+                              ],
+                              '$status',
+                            ],
+                          },
+                        ],
+                      },
                     },
                   },
+                  as: 'status',
+                  in: { status: '$$status' },
                 },
-                as: 'status',
-                in: { status: '$$status' },
               },
             },
           },
-        },
-        {
-          $lookup: {
-            from: 'users',
-            localField: 'customer',
-            foreignField: '_id',
-            as: 'customerInfo',
-          },
-        },
-        {
-          $unwind: '$customerInfo',
-        },
-        {
-          $lookup: {
-            from: 'agents',
-            localField: 'assignedAgent',
-            foreignField: '_id',
-            as: 'agentInfo',
-          },
-        },
-        {
-          $unwind: '$agentInfo',
-        },
-        {
-          $project: {
-            trackingNumber: 1,
-            status: 1,
-            'deliveryAddress.street': 1,
-            'deliveryAddress.city': 1,
-            'deliveryAddress.state': 1,
-            'deliveryAddress.country': 1,
-            'deliveryAddress.postalCode': 1,
-            'deliveryAddress.coordinates': 1,
-            'parcelDetails.size': 1,
-            'payment.type': 1,
-            'payment.status': 1,
-            nextStatuses: 1,
-            customer: {
-              personalInfo: '$customerInfo.personalInfo',
-              location: '$customerInfo.location',
-            },
-            agent: {
-              // personalInfo: '$agentInfo.personalInfo',
-              location: '$agentInfo.location',
+          {
+            $lookup: {
+              from: 'users',
+              localField: 'customer',
+              foreignField: '_id',
+              as: 'customerInfo',
             },
           },
-        },
-      ]);
+          {
+            $unwind: '$customerInfo',
+          },
+          {
+            $lookup: {
+              from: 'agents',
+              localField: 'assignedAgent',
+              foreignField: '_id',
+              as: 'agentInfo',
+            },
+          },
+          {
+            $unwind: '$agentInfo',
+          },
+          {
+            $project: {
+              trackingNumber: 1,
+              status: 1,
+              deliveryAddress: 1,
+              parcelDetails: 1,
+              payment: 1,
+              nextStatuses: 1,
+              customer: {
+                personalInfo: '$customerInfo.personalInfo',
+                location: '$customerInfo.location',
+              },
+              agent: {
+                // personalInfo: '$agentInfo.personalInfo',
+                'location.coordinates': '$agentInfo.location.coordinates',
+              },
+            },
+          },
+        ])
+        .skip(skip)
+        .limit(limit);
 
       res.status(HttpStatusCode.OK).json({
         status: Status.SUCCESS,
         data: {
+          limit: 10,
+          total: parcels.length,
           parcels,
         },
       });
@@ -132,31 +135,28 @@ export class ParcelAgentServices {
 
   public findOneAndUpdateStatus: RequestHandler = catchAsync(
     async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-      console.log(req.body);
+      const { status } = req.body;
 
       const parcel = await this.model.findOneAndUpdate(
         {
           $and: [
             { trackingNumber: req.params.trackingNumber },
-            {
-              status: {
-                $nin: ['Booked', 'Failed'],
-              },
-            },
+            { status: { $nin: ['Booked', 'Failed'] } },
             { assignedAgent: req.self.id },
           ],
         },
         {
-          $set: {
-            status: req.body.status,
+          $set: { status },
+          $push: {
+            trackingHistory: {
+              status,
+              timestamp: new Date(),
+              notes: `Status updated to ${status}`,
+            },
           },
         },
         {
-          writeConcern: {
-            w: 'majority',
-            j: true,
-            wtimeout: 5000,
-          },
+          writeConcern: { w: 'majority', j: true, wtimeout: 5000 },
           new: true,
         }
       );
